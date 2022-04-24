d51c69af7c133e2356cc338727b32ae4d6766ab5
eraldovicente <eraldovicente2@gmail.com>
2022-04-24 16:39:26 -0300

    Prática bloques anónimos y variables

--- aula-01.sql
+++ aula-01.sql
@@ -1,12 +1,52 @@
 -------------------
 -- BLOCO ANÔNIMO --
 -------------------
+
+---------------
+-- QUESTÃO 1 --
+---------------
+
 SET SERVEROUTPUT ON
 
 DECLARE
-    X CONSTANT NUMBER := 10;
-    Z NUMBER NOT NULL := 20;
+    nome      VARCHAR2 := 'Eraldo';
+    sobrenome VARCHAR2 := 'Vicente';
 BEGIN
-    DBMS_OUTPUT.PUT_LINE(X);
-    DBMS_OUTPUT.PUT_LINE(Z);
+    dbms_output.put_line(nome);
+    dbms_output.put_line(sobrenome);
+    dbms_output.put_line(nome
+                         || ' '
+                         || sobrenome);
+END;
+
+---------------
+-- QUESTÃO 2 --
+---------------
+
+-- 4
+
+---------------
+-- QUESTÃO 3 --
+---------------
+
+DECLARE
+  N1 NUMBER := 1;
+  N2 NUMBER := 1;
+BEGIN
+  DBMS_OUTPUT.PUT_LINE(N1 + N2);
+END;
+
+---------------
+-- QUESTÃO 4 --
+---------------
+
+DECLARE
+  N1 NUMBER;
+  N2 NUMBER;
+  RESULTADO NUMBER;
+BEGIN
+  N1 := NULL;
+  N2 := 1;
+  RESULTADO := N1 + N2;
+  DBMS_OUTPUT.PUT_LINE('O RESULTADO DA SOMA: ' || RESULTADO);
 END;
