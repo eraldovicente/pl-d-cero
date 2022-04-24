41719faba49f0b650dfc878a14301750958e5800
eraldovicente <eraldovicente2@gmail.com>
2022-04-24 17:07:05 -0300

    Variables boolean

--- aula-01.sql
+++ aula-01.sql
@@ -2,51 +2,13 @@
 -- BLOCO ANÔNIMO --
 -------------------
 
----------------
--- QUESTÃO 1 --
----------------
-
 SET SERVEROUTPUT ON
 
 DECLARE
-    nome      VARCHAR2 := 'Eraldo';
-    sobrenome VARCHAR2 := 'Vicente';
+    B1 BOOLEAN;
 BEGIN
-    dbms_output.put_line(nome);
-    dbms_output.put_line(sobrenome);
-    dbms_output.put_line(nome
-                         || ' '
-                         || sobrenome);
+    B1 := TRUE;
+    B1 := FALSE;
+    B1 := NULL;
 END;
 
----------------
--- QUESTÃO 2 --
----------------
-
--- 4
-
----------------
--- QUESTÃO 3 --
----------------
-
-DECLARE
-  N1 NUMBER := 1;
-  N2 NUMBER := 1;
-BEGIN
-  DBMS_OUTPUT.PUT_LINE(N1 + N2);
-END;
-
----------------
--- QUESTÃO 4 --
----------------
-
-DECLARE
-  N1 NUMBER;
-  N2 NUMBER;
-  RESULTADO NUMBER;
-BEGIN
-  N1 := NULL;
-  N2 := 1;
-  RESULTADO := N1 + N2;
-  DBMS_OUTPUT.PUT_LINE('O RESULTADO DA SOMA: ' || RESULTADO);
-END;
