7ebbf076ebe9ee78a891f344144bbbc6af9fd21f
eraldovicente <eraldovicente2@gmail.com>
2022-05-05 13:56:29 -0300

    COMANDO GOTO

--- aula-01.sql
+++ aula-01.sql
@@ -49,6 +49,27 @@ BEGIN
         dbms_output.put_line('HE PASADO POR AQUI');
         done := true;
     END LOOP;
+END;
+/
 
+------------------
+-- COMANDO GOTO --
+------------------
+
+DECLARE
+    P VARCHAR2(30);
+    N PLS_INTEGER := 5;
+BEGIN
+    FOR J IN 2..ROUND(SQRT(N)) LOOP
+        IF N MOD J = 0 THEN
+            P := ' NO ES UN NÚMERO PRIMO';
+            GOTO PRINT_NOW;
+        END IF;
+    END LOOP;
+    
+    P := ' ES UN NÚMERO PRIMO';
+    
+    <<PRINT_NOW>>
+    DBMS_OUTPUT.PUT_LINE(TO_CHAR(N) || P);
 END;
 /
