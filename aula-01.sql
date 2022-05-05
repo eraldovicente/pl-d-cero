40217c4d2e05958aa01d599c29613bceb26db359
eraldovicente <eraldovicente2@gmail.com>
2022-05-05 14:45:09 -0300

    %ROWTYPE

--- aula-01.sql
+++ aula-01.sql
@@ -97,3 +97,24 @@ BEGIN
     dbms_output.put_line(salario);
     dbms_output.put_line(nombre);
 END;
+
+--------------
+-- %ROWTYPE --
+--------------
+
+DECLARE
+    salario  NUMBER;
+    nombre   employees.first_name%TYPE;
+    empleado employees%rowtype;
+BEGIN
+    SELECT -- SOLO PUEDE DEVOLVER UNA FILA
+        *
+    INTO empleado
+    FROM
+        employees
+    WHERE
+        employee_id = 100;
+
+    dbms_output.put_line(empleado.salary * 100);
+    dbms_output.put_line(empleado.first_name);
+END;
