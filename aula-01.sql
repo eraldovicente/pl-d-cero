dd36f24bb9c35229f37a29105472efe41e7ddafb
eraldovicente <eraldovicente2@gmail.com>
2022-05-05 12:08:38 -0300

    BUCLE WHILE

--- aula-01.sql
+++ aula-01.sql
@@ -16,12 +16,39 @@ END;
 -- Bucle FOR --
 ---------------
 set SERVEROUTPUT ON
-DECLARE 
-    I VARCHAR2(100) := 'AAAAAA';
+
+DECLARE
+    i VARCHAR2(100) := 'AAAAAA';
 BEGIN
     FOR i IN REVERSE 5..15 LOOP     -- PLS_INTEGER
         dbms_output.put_line(i);
-        exit when i=10;
+        EXIT WHEN i = 10;
     END LOOP;
+
     dbms_output.put_line(i);
 END;
+
+-----------------
+-- BUCLE WHILE --
+-----------------
+
+DECLARE
+    done BOOLEAN := false;
+    X NUMBER := 0;
+BEGIN
+    WHILE X < 10 LOOP
+        DBMS_OUTPUT.PUT_LINE(X);
+        X := X + 1;
+        EXIT WHEN X = 5;
+    END LOOP;
+    WHILE done LOOP
+        dbms_output.put_line('NO IMPRIMAS ESTO.');
+        done := true;
+    END LOOP;
+    WHILE NOT done LOOP
+        dbms_output.put_line('HE PASADO POR AQUI');
+        done := true;
+    END LOOP;
+
+END;
+/
