20ec2d4b5742d8c1683e3f05412e875f8a885979
eraldovicente <eraldovicente2@gmail.com>
2022-05-05 11:49:49 -0300

    BUBLE FOR

--- aula-01.sql
+++ aula-01.sql
@@ -5,10 +5,23 @@
 SET SERVEROUTPUT ON
 
 DECLARE
-    B1 BOOLEAN;
+    b1 BOOLEAN;
 BEGIN
-    B1 := TRUE;
-    B1 := FALSE;
-    B1 := NULL;
+    b1 := true;
+    b1 := false;
+    b1 := NULL;
 END;
 
+---------------
+-- Bucle FOR --
+---------------
+set SERVEROUTPUT ON
+DECLARE 
+    I VARCHAR2(100) := 'AAAAAA';
+BEGIN
+    FOR i IN REVERSE 5..15 LOOP     -- PLS_INTEGER
+        dbms_output.put_line(i);
+        exit when i=10;
+    END LOOP;
+    dbms_output.put_line(i);
+END;
