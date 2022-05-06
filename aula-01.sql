350ce918013c84180cc5582e927aafdc62ed5ebf
eraldovicente <eraldovicente2@gmail.com>
2022-05-06 18:14:35 -0300

    INSERTS Y UPDATES CON PL/SQL RECORDS

--- aula-01.sql
+++ aula-01.sql
@@ -461,3 +461,47 @@ BEGIN
     dbms_output.put_line(emple1.fecha);
     dbms_output.put_line(emple1.datos.first_name);
 END;
+
+-----------------------------------------
+--INSERTS Y UPDATES CON PL/SQL RECORDS --
+-----------------------------------------
+
+CREATE TABLE regiones
+    AS
+        SELECT
+            *
+        FROM
+            regions
+        WHERE
+            region_id = 0;
+
+DECLARE
+    reg1 regions%rowtype;
+BEGIN
+    SELECT
+        *
+    INTO reg1
+    FROM
+        regions
+    WHERE
+        region_id = 1;
+    
+    -- INSERT
+    INSERT INTO regiones VALUES reg1;
+
+END;
+/
+
+DECLARE
+    reg1 regions%rowtype;
+BEGIN
+    reg1.region_id := 1;
+    reg1.region_name := 'AUSTRALIA';
+    --UPDATE
+    UPDATE regiones
+    SET
+        row = reg1
+    WHERE
+        region_id = 1;
+
+END;
