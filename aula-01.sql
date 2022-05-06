a486a27f9a76a06916712e1ab61ddc5b691b5a34
eraldovicente <eraldovicente2@gmail.com>
2022-05-05 22:09:10 -0300

    CONTROLAR SQL CON EXCEPCIONES

--- aula-01.sql
+++ aula-01.sql
@@ -307,3 +307,32 @@ EXCEPTION
         );
 
 END;
+
+-----------------------------------
+-- CONTROLAR SQL CON EXCEPCIONES --
+-----------------------------------
+
+DECLARE
+    reg         regions%rowtype;
+    reg_control regions.region_id%TYPE;
+BEGIN
+    reg.region_id := 100;
+    reg.region_name := 'AFRICA';
+    SELECT
+        region_id
+    INTO reg_control
+    FROM
+        regions
+    WHERE
+        region_id = reg.region_id;
+
+    dbms_output.put_line('LA REGION YA EXISTE');
+EXCEPTION
+    WHEN no_data_found THEN
+        INSERT INTO regions VALUES (
+            reg.region_id,
+            reg.region_name
+        );
+
+        COMMIT;
+END;
