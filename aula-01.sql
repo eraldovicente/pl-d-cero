af24beaf9af1a7f7aea972e1b970ffd4e642ce9d
eraldovicente <eraldovicente2@gmail.com>
2022-05-08 21:24:04 -0300

    CURSORES CON PARÁMETROS

--- aula-01.sql
+++ aula-01.sql
@@ -717,3 +717,23 @@ BEGIN
         DBMS_OUTPUT.PUT_LINE(I.REGION_NAME);
     END LOOP;
 END;
+
+-----------------------------
+-- cursores con parámetros --
+-----------------------------
+
+DECLARE
+    CURSOR C1(SAL NUMBER) IS SELECT * FROM EMPLOYEES
+    WHERE SALARY > SAL;
+    
+    EMPL EMPLOYEES%ROWTYPE;
+BEGIN
+    OPEN C1(8000);
+    LOOP
+        FETCH C1 INTO EMPL;
+        EXIT WHEN C1%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(EMPL.FIRST_NAME||' '||EMPL.SALARY);
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('HE ENCONTRADO '||C1%ROWCOUNT||' EMPLEADOS');
+    CLOSE C1;
+END;
